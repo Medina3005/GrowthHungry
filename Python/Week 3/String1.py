df38d94ed10972ba7dd918ab772a7eb74bbeadec
Medina <kubanycbekovam87@gmail.com>
2025-10-30 21:54:40 -0500

    Updated the String1.py

--- Python/Week 3/String1.py
+++ Python/Week 3/String1.py
@@ -23,9 +23,13 @@
 # instead of the actual count.
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
-def donuts(count):
-  # +++your code here+++
-  return
+
+def donuts(count):    
+  if count >= 10:
+    mystring = 'many'
+  else:
+    mystring = str(count)
+    return 'Number of donuts: ' + mystring
 
 
 # B. both_ends
@@ -33,9 +37,12 @@ def donuts(count):
 # and the last 2 chars of the original string,
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
+
 def both_ends(s):
-  # +++your code here+++
-  return
+    if len(s) < 2:
+        return ''
+    else:
+        return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -48,8 +55,9 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  char = s[0]
+  result = char + s[1:].replace(char, '*')
+  return result
 
 
 # D. MixUp
@@ -60,8 +68,9 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  str1 = b[:2] + a[2:]
+  str2 = a[:2] + b[2:]
+  return str1 + ' ' + str2
 
 
 # Provided simple test() function used in main() to print
