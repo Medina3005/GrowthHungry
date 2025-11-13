bdd36fc1a2f1abe248ff93f3ec821f8ea0971551
Medina <kubanycbekovam87@gmail.com>
2025-11-12 18:25:31 -0600

    Updated the .gitkeep file

---
+++ Python/Week 4/AdvancedTipCalculator.py
@@ -0,0 +1,21 @@
+# Project:
+# Upgrade: Advanced Tip Calculator. Modify last week's Tip Calculator. Add a feature that asks how many people are splitting the bill and their names.
+# The program should then calculate and display how much each person needs to pay.
+# Add a condition: if the tip percentage is over 20%, print a "Thank you for your generosity!" message.
+
+
+name = input("Enter your name: ")
+print(name)
+
+total_amount = float(input("Enter total amount:"))
+print(total_amount)
+tip_percent = int(input("Please choose your tip %: "))
+print(tip_percent)
+
+
+tip = (total_amount * tip_percent) / 100
+total_bill = total_amount + tip
+total_bill = round(total_bill, 2)
+print("Your total bill amount is:", total_bill, "with", str(tip_percent) + "%", "tip:", tip)
+
+
