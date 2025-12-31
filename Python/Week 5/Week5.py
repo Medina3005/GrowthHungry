cba780a9e3d1c67c03a2b47d859d3a3907b5f82a
Medina <kubanycbekovam87@gmail.com>
2025-12-31 02:19:37 -0600

    added week4.txt notes

---
+++ Python/Week 5/Week5.py
@@ -0,0 +1,21 @@
+# LOOPS
+
+# Repetitive Tasks with Loops:- For loop and using range()- While loop- Break and continue
+# Project:
+# Number Guessing Game. The program chooses a secret random number between 1 and 100. The user has a limited number of attempts to guess it.
+# After each guess, the program tells the user if their guess was "too high," "too low," or "correct."
+# Project:
+# Simple To-Do List. Create a program that allows a user to manage a to-do list. The user should be able to:
+# Add a new task.
+# View all tasks.
+# Delete a task by its name or number.
+# The program should loop until the user decides to quit.
+
+# go from the beginning to the end. -> iterate. 
+
+# schools = [2,3,5,7]
+# for school in schools:
+#     print(school)
+
+
+# While - you can iterate but you don't when it will end. 
--- script.py
+++
@@ -1,7 +0,0 @@
-echo "Hello World"
-x=""my name is Dina"
-echo '$x'
-echo "$x"
-echo "${x}"
-echo "${x:0:5}"
-echo "${#x}"
